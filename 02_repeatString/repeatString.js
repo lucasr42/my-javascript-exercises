df6286ca7774b39e022503bcafe94fc6625085aa
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-16 21:22:00 -0700

    Passed these ones too!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    }
+    let newStr = str;
+    for (let i = 0; i < num-1; i++) {
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
