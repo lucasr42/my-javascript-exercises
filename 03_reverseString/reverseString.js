b3e4ce0c410c4fae59c8df9be04c98c13e22220a
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-16 21:47:33 -0700

    Reverses a string and uses the reduce method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    if (word.length === 0) return '';
+    if (word.length === 1) return word;
+    
+    let splitWord = [...word];
+    let length = splitWord.length-1;
 
+    for (let i = 0; i <= length/2; i++) {
+        const temp = splitWord[i];
+        splitWord[i] = splitWord[length-i];
+        splitWord[length-i] = temp;
+    }
+    
+    return splitWord.reduce((acc, cur) => acc + cur);
 };
 
 // Do not edit below this line
