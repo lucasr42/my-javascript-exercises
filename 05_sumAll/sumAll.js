6307b1b4da6946e16c1a5161a88ade40dfbbb571
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-22 09:26:00 -0700

    Works to add the nums and catch the not nums. There's probably a better way though

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || 
+        num2 < 0 || 
+        !Number.isInteger(num1) || 
+        !Number.isInteger(num2)) return "ERROR";
+    
+    let smaller;
+    let larger;
+    if (num1 < num2) {
+        smaller = num1;
+        larger = num2;
+    } else {
+        smaller = num2;
+        larger = num1;
+    }
 
+    for (let i = smaller+1; i <= larger; i++) {
+        smaller += i;
+    }
+
+    return smaller;
 };
 
 // Do not edit below this line
