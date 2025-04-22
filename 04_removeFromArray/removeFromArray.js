63c796fbb0e6fe831241685c82bc925de0825ee4
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-22 09:04:17 -0700

    Removed the items. Used the built in arguments attr to access params

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
+
+/**
+ * Uses the builtin "arguments" attribute of all Javascript functions to allow any number of 
+ * arguments to be passed and to access their values.
+ *
+ * @returns An array with items removed
+ */
 const removeFromArray = function() {
+    const arr = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        const foundIndex = arr.findIndex((value, index, arr) => value === arguments[i]);
+        if (foundIndex !== -1) {
+            arr.splice(foundIndex, 1);
+            i--; // Look again to account for shrinking array size
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
