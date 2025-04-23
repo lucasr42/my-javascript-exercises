91add2bb92cbeee35d6ca42843f67546f46d7355
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-23 11:00:02 -0700

    Did the math. Used a couple different fun methods: recursion, reduce

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    // let sum = 0;
+	// for (let i = 0; i < nums.length; i++) {
+    //     sum += nums[i];
+    // }
+    // return sum;
+    if (nums.length === 0) return 0;
+    return nums.reduce((sum, num) => {
+        return sum + num;
+    })
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    if (nums.length === 0) return 0;
+    return nums.reduce((prod, num) => {
+        return prod * num;
+    })
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    let res = 1;
+    for (let i = 0; i < num2; i++) {
+        res *= num1;
+    }
+    return res;
+    // return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+    console.log("num: ", num);
+    return num *= factorial(num-1);
+    // let res = 1;
+    // for (let i = 1; i <= num; i++) {
+    //     res *= i;
+    // }
+    // return res;
 };
 
 // Do not edit below this line
