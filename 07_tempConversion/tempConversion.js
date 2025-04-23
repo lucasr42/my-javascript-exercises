654514b99cfec125c90e304de13881f0a1d2a748
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-23 10:31:36 -0700

    Can also use Math.round()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,8 @@
 const convertToCelsius = function(temp) {
     // c = (f-32) * 1/1.8
     if (isNaN(temp)) return -1;
-    const celsius = parseFloat(((temp-32) * (1/1.8)).toFixed(1));
+    // const celsius = parseFloat(((temp-32) * (1/1.8)).toFixed(1));
+    const celsius = Math.round(((temp-32) * (1/1.8)) * 10) / 10;
     console.log(celsius);
     return celsius;
 };
@@ -9,7 +10,8 @@ const convertToCelsius = function(temp) {
 const convertToFahrenheit = function(temp) {
     // f = c*1.8 + 32
     if (isNaN(temp)) return -1;
-    const fahrenheit = parseFloat((temp * 1.8 + 32).toFixed(1));
+    // const fahrenheit = parseFloat((temp * 1.8 + 32).toFixed(1));
+    const fahrenheit = Math.round((temp * 1.8 + 32) * 10) / 10;
     console.log(fahrenheit);
     return fahrenheit;
 };
