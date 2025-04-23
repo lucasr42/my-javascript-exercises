705624acdcea7dcdaa965e495b391ce7c353b6ad
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-23 09:49:45 -0700

    Solved the leap years. Nothing crazy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return false;
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
 // Do not edit below this line
