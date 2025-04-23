c94c52967b7951a84ed082d4ef8818fd189f717c
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-04-23 10:28:07 -0700

    Converted the temps. toFixed returns a string so you have to convert it back

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    // c = (f-32) * 1/1.8
+    if (isNaN(temp)) return -1;
+    const celsius = parseFloat(((temp-32) * (1/1.8)).toFixed(1));
+    console.log(celsius);
+    return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    // f = c*1.8 + 32
+    if (isNaN(temp)) return -1;
+    const fahrenheit = parseFloat((temp * 1.8 + 32).toFixed(1));
+    console.log(fahrenheit);
+    return fahrenheit;
 };
 
 // Do not edit below this line
