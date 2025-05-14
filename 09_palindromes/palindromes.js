53ffbac8d49af7ef3ef401ea7e25748fc8259c4c
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-05-13 20:58:01 -0700

    Still using a regular for loop cause it makes sense

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,50 @@
-const palindromes = function () {
+const isValidChar = function (char) {
+    // Check if character is one of the following:
+    // - lowercase letter
+    // - number
+    // - Not a space
+    const ascii = char.charCodeAt(0);
+    let ans = true;
+    if ((ascii < 48 || ascii > 57) && (ascii < 97 || ascii > 122)) {
+        console.log(`Char ${char} is not valid letter or number`);
+        ans = false;
+    } else if (ascii == 32) {
+        console.log(`Char ${char} is a space`);
+        ans = false;
+    }
+    console.log(`Is char: ${char} valid? ${ans}`);
+    return ans;
+}
 
+const palindromes = function (word) {
+    console.log(`Testing: ${word}`);
+    // 1. normal
+    // 2. ignore punctuation
+    // 3. make everything lowercase to count it
+    // 4. ignore spaces
+    // 5. numbers count if they match
+    let end = word.length - 1;
+    for (let i = 0; i < word.length/2; i++) {
+        let first = word[i].toLowerCase();
+        let last = word[end].toLowerCase();
+
+        while (!isValidChar(first)) {
+            console.log(`first letter - word[${i}] = ${word[i]}`);
+            i++;
+            first = word[i];
+        }
+        while (!isValidChar(last)) {
+            console.log(` last letter - word[${i}] = ${word[i]}`)
+            end--;
+            last = word[end];
+        }
+
+        if (first !== last) {
+            return false;
+        }
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
