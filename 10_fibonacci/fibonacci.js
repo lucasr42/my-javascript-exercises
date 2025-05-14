2b40a70875eed12e378f0290a5085c60099caada
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-05-14 06:54:24 -0700

    Made it work with another for loop again

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const cleanNum = parseInt(num);
+    if (cleanNum === 0) return 0;
+    if (cleanNum < 0) return "OOPS";
+    
+    const arr = [1, 1];
+    for (let i = 1; i < cleanNum-1; i++) {
+        const nextNum = arr[i] + arr[i-1];
+        console.log(nextNum);
+        arr.push(nextNum);
+        console.log(arr.toString());
+    }
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
