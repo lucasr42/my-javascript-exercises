e3e7d5143b63144377a974277465e4a53df007f8
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-05-14 17:44:36 -0700

    Found the oldest by sorting them by age with toSorted

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function calculateAge(person) {
+        if (person.yearOfDeath) {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+        const currYear = 2025;
+        const age = currYear - person.yearOfBirth;
 
+        return age;
+    }
+
+    const sortedByAge = people.toSorted((person1, person2) => {
+        const age1 = calculateAge(person1);
+        const age2 = calculateAge(person2);
+        return age1 - age2;
+    })
+
+    return sortedByAge[sortedByAge.length-1];
 };
 
 // Do not edit below this line
