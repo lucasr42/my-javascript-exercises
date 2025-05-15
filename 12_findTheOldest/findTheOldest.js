41d9d5a17c387a45225e46538e6e7e890645acc8
Lucas Robinson <lucas.robinson42@hotmail.com>
2025-05-14 17:52:23 -0700

    This is using reduce like that showed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -9,13 +9,18 @@ const findTheOldest = function(people) {
         return age;
     }
 
-    const sortedByAge = people.toSorted((person1, person2) => {
-        const age1 = calculateAge(person1);
-        const age2 = calculateAge(person2);
-        return age1 - age2;
-    })
+    // const sortedByAge = people.toSorted((person1, person2) => {
+    //     const age1 = calculateAge(person1);
+    //     const age2 = calculateAge(person2);
+    //     return age1 - age2;
+    // })
 
-    return sortedByAge[sortedByAge.length-1];
+    // return sortedByAge[sortedByAge.length-1];
+    return people.reduce((oldest, curr) => {
+        const age1 = calculateAge(oldest);
+        const age2 = calculateAge(curr);
+        return age1 < age2 ? curr : oldest;
+    });
 };
 
 // Do not edit below this line
